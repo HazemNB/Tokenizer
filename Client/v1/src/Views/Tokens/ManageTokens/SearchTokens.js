11a34942e2b3c4deb3463fd61fe6b403043f0886
Mahmoud123Makhaly <mahmoudmakhaly123@gmail.com>
2023-03-08 21:49:37 +0200

    search tokens

--- Client/v1/src/Views/Tokens/ManageTokens/SearchTokens.js
+++ Client/v1/src/Views/Tokens/ManageTokens/SearchTokens.js
@@ -1,48 +1,53 @@
-import React, { useState } from 'react'
 import { Card } from '@mui/material'
-import SoftBox from "components/SoftBox";
-import SoftTypography from "components/SoftTypography";
-import SoftInput from "components/SoftInput";
-import SoftButton from "components/SoftButton";
-import TokenTypeSelector from 'ProjectComponents/Selectors/TokenTypeSelector';
-const SearchTokens = ({SearchReq,setSearchReq}) => {
-    const [TemplateId, setTemplateId] = useState();
-    const[TokenType , setTokenType]=useState();
-    const[IdFrom , setIdFrom]=useState();
-    const[IdTo , setIdTo]=useState();
-    const[Url , setUrl]=useState()
-    const[CreatedAtFrom,setCreatedAtFrom]=useState();
-    const[Amount,setAmount]=useState();
-    const[LastUpdated,setLastUpdated]=useState();
-    const[Claimed , setClaimed]=useState();
-    const[Redeemed,setRedeemed]=useState();
+import SoftBox from 'components/SoftBox'
+import SoftButton from 'components/SoftButton'
+import SoftInput from 'components/SoftInput'
+import SoftTypography from 'components/SoftTypography'
+import TokenTypeSelector from 'ProjectComponents/TokenTypeSelector'
+import React, { useState } from 'react';
+import "./ManageTokens.scss"
+
+const SearchTokens = ({SearchReq , setSearchReq}) => {
+    const [TemplateId , setTemplateId] = useState();
+    const [TokenType,setTokenType ]=useState();
+    const[IdFrom,setIdFrom] = useState();
+    const [IdTo  , setIdTo] = useState();
+    const[Url , setUrl] = useState();
+    const [CreatedAtFrom,setCreatedAtFrom] = useState();
+    const[Amount , setAmount] = useState();
+    const[LastUpdated,setLastUpdated] = useState();
+    const [Claimed,setClaimed] = useState();
+    const [Redeemed,setRedeemed] = useState();
     const[IsActive,setIsActive]=useState();
-const searchUsers = async()=>{
-    let req = Object.assign({}, SearchReq);
-    req.TemplateId=TemplateId;
-    req.TokenTypeId=TokenType.id;
-    req.IdFrom=IdFrom;
-    req.IdTo=IdTo;
-    req.Url=Url;
-    req.CreatedAtFrom=CreatedAtFrom;
-    req.Amount=Amount;
-    req.LastUpdated=LastUpdated;
-    req.Claimed=Claimed;
-    req.Redeemed=Redeemed;
-    req.IsActive=IsActive;
-    setSearchReq(req);
-}
-    
-  return (
-    <Card className="searchUsersCard">
-    <SoftBox pt={3} px={2}>
+    const [CompanyId,setCompanyId] = useState()
+
+    const searchUsers = async()=>{
+        let req = Object.assign({}, SearchReq);
+        req.TemplateId=TemplateId;
+        req.TokenTypeId=TokenType;
+        req.IdFrom=IdFrom;
+        req.IdTo=IdTo;
+        req.Url=Url;
+        req.CreatedAtFrom=CreatedAtFrom;
+        req.Amount=Amount;
+        req.LastUpdated=LastUpdated;
+        req.Claimed=Claimed;
+        req.Redeemed=Redeemed;
+        req.IsActive=IsActive;
+        req.CompanyId = CompanyId;
+        setSearchReq(req);
+    }
+    return (
+        <Card className="searchUsersCard">
+            <SoftBox pt={3} px={2}>
                 <SoftTypography variant="h6" fontWeight="medium">
                     Search Tokens
                 </SoftTypography>
             </SoftBox>
             <SoftBox px={2} py={3} sx={{ display: 'grid', flexDirection: 'column', gridTemplateColumns: { sm: "1fr 1fr 1fr", xs: "1fr" }, gridColumnGap: "2em", gridRowGap: "1em" }} className="SearchUsersInputs">
             <SoftInput placeholder="Template Id" type="number" onChange={(e) => setTemplateId(e.target.value)} />
-   <TokenTypeSelector setTokenType={setTokenType} style={{marginTop:"0"}}/>
+            <SoftInput placeholder="Company Id" type="number" onChange={(e) => setCompanyId(e.target.value)} />
+            <TokenTypeSelector setTokenType={setTokenType}/>
             <SoftInput placeholder="Id From" type="text" onChange={(e) => setIdFrom(e.target.value)} />
             <SoftInput placeholder="Id To" type="text" onChange={(e) => setIdTo(e.target.value)} />
             <SoftInput placeholder="Url" type="text" onChange={(e) => setUrl(e.target.value)} />
@@ -55,14 +60,14 @@ const searchUsers = async()=>{
                     <input type="checkbox" onChange={(e) => setIsActive(e.target.checked)} />
                     <label>Active</label>
                 </div>
-             </SoftBox>
-             <div style={{ textAlign: "center" }}>
+            </SoftBox>
+            <div style={{ textAlign: "center" }}>
                 <SoftButton variant="contained" color="info" sx={{ width: "80%", marginBottom: "1em" }} onClick={searchUsers}>
                     Search
                 </SoftButton>
             </div>
         </Card>
-  )
+    )
 }
 
 export default SearchTokens
